76860200b2461b5c1dc1ebd3044f6f83cda485c2
OsamaIbrahim1 <oi5921435@gmail.com>
2022-09-06 07:02:05 +0200

    comit

---
+++ demo7.js
@@ -0,0 +1,56 @@
+
+
+
+
+
+// function clickhandler(){
+// console.log("btn click");
+// }
+// let myAdmins = ["Ahmed", "Osama", "Sayed", "Mohamed","Stop", "Samera"];
+// let myEmployees = ["Amgad", "Samah", "Ameer", "Omar","Mohamed","Mahmoud", "Othman", "Amany", "Samia"];
+// // let s = myAdmins.slice(0, 3);
+
+// for (let i = 0; i < myAdmins.length; i++) {
+//     if (myAdmins[i] === "Stop") {
+//         myAdmins.length = i;
+//         break;
+//     }
+// }
+// document.write(`<h1>we have ${myAdmins.length} Admins</h1>`);
+
+// for (let i = 0; i < myAdmins.length; i++) {
+//     document.write(`<hr>`);
+//     document.write(`<div>`);
+//     document.write(`The Admin For Team ${i + 1} Is ${myAdmins[i]}`);
+//     document.write(`<h3>Team Member:</h3>`);
+// let m=0;
+//     for (let j = 0; j < myEmployees.length; j++) {
+//         if (myAdmins[i][0] === myEmployees[j][0])
+//             document.write(`<br>- ${++m} ${myEmployees[j]}<br> `);
+//     }
+//     document.write(`</div>`);
+// }
+// let productes = ["keyboard" , "mouse" , "Pen" ,"pad","monitor","iphone"];
+// let colors = ["red" , "green" , "blue"];
+// let showcount = 2;
+
+// document.write(`<h1>show ${showcount} products</h1>`);
+// for(let i=0;i<showcount;i++) {
+//     document.write(`<div>`);
+//     document.write(`<p>[${i+1}] ${productes[i]}</p>`);
+//     document.write(`<p>colors </p>`);
+//     for(let j=0;j<colors.length;j++) {
+//         document.write(`<p>${colors[j]}</p>`);
+//     }
+//     document.write(`<p>${colors.join(" | ")}</p>`);
+//     document.write(`</div>`);
+// }
+// var num = parseInt(prompt("enter number"));
+// if (num % 3 == 0)
+//     document.write("Fizz");
+// else if (num % 5 == 0)
+//     document.write("Buzz");
+// else if (num % 5 == 0 && num % 3 == 0)
+//     document.write("FizzBuzz");
+// else
+//     document.write("none");
